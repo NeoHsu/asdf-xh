d9166259e63f3f04e43b740a48edda3c11e319a4
Neo Hsu <NeoHsuDroid@gmail.com>
2021-07-04 19:28:42 +0800

    docs(comment): remove TODO comment

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for xh.
 GH_REPO="https://github.com/ducaale/xh"
 TOOL_NAME="xh"
 TOOL_TEST="xh --help"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if xh has other means of determining installable versions.
   list_github_tags
 }
 
