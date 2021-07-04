c39d67e2cd66c60899a6d3eef70064beca8d65c0
Neo Hsu <NeoHsuDroid@gmail.com>
2021-07-04 19:27:11 +0800

    style(lint): fix lint format

--- lib/utils.bash
+++ lib/utils.bash
@@ -72,7 +72,7 @@ install_version() {
 
     mkdir -p "$install_path/bin"
     cp "$ASDF_DOWNLOAD_PATH/$TOOL_NAME$ext" "$install_path/bin/$TOOL_NAME$ext"
-  
+
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
